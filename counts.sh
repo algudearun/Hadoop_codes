2f66116ce5260b752232cc77e63c1ba8df03cd19
algudearun <39325764+algudearun@users.noreply.github.com>
2020-10-14 15:04:26 +0530

    Update counts.sh

--- counts.sh
+++ counts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "" > 1output.txt
-hadoop fs -count -q -v /user/hive/warehouse/*/udm_p* | tr -s " " | while read var; do
+hadoop fs -count -q -v /user/hive/warehouse/* | tr -s " " | while read var; do
 count_var=$(echo $var | cut -d " " -f6)
 (( "${count_var}" >= "1000" )) && echo $var | cut -f6,8 -d " " >> 1output.txt
 done
